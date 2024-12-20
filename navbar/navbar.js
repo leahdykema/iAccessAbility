15bbcfa33b05e7652abf8eff7b3bf89924f80e93
Leah Dykema <103963317+leahdykema@users.noreply.github.com>
2024-12-19 20:45:26 -0600

    Update navbar.js

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,8 +1,11 @@
-// mobile versions
-const menuIcon = document.querySelector('.menu-icon-container');
+const menuIcon = document.querySelector(".menu-icon-container");
 const navContainer = document.querySelector(".nav-container");
+const desktopNav = document.querySelector(".desktop-nav");
+const mobileNav = document.querySelector(".mobile-nav");
 
 menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle('active');
-    navContainer.classList.toggle('active');
+    menuIcon.classList.toggle("active");
+    navContainer.classList.toggle("active");
+    desktopNav.classList.toggle("hidden"); // Toggle visibility of desktop nav
+    mobileNav.classList.toggle("hidden"); // Optional, depending on design
 });
