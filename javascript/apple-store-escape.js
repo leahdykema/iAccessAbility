27615971525a615c69fa8abed05ee632ef91586e
Leah Dykema <103963317+leahdykema@users.noreply.github.com>
2025-03-10 15:00:56 -0500

    Update apple-store-escape.js
    Signed-off-by: Leah Dykema <103963317+leahdykema@users.noreply.github.com>

--- javascript/apple-store-escape.js
+++ javascript/apple-store-escape.js
@@ -21,9 +21,11 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("storePhone").style.display = "block";
         } else if (gameState.stage === 3) {
             document.getElementById("breakGlass").style.display = "block";
-            document.getElementById("dj").style.display = "block";
+            document.getElementById("backDoor").style.display = "block";
+            document.getElementById("frontDoor").style.display = "block";
         } else if (gameState.stage === 4) {
             document.getElementById("ceilingVent").style.display = "block";
+            document.getElementById("dj").style.display = "block";
             document.getElementById("callCook").style.display = "block";
         } else if (gameState.stage === 5 || gameState.stage === 6) {
             // Remove buttons when game ends
@@ -64,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     function frontEntrance() {
-        logMessage("You walk up to the entrance doors and pull on the handle, but it doesn't budge. You try pushing, but same thing, it rattles but stays firmly shut.");
+        logMessage("You walk up to the entrance doors and pull on the handle, but it doesn't budge. You try pushing it and the door rattles but stays firmly shut.");
         gameState.hasEasyPayAccess = true;
         progressStage(2);
     }
@@ -88,12 +90,17 @@ document.addEventListener("DOMContentLoaded", () => {
     
     // stage 3
     function breakGlass() {
-        logMessage("You try to break the glass, but it's Apple Store glass... impossible.");
+        logMessage("You try to break the glass, but since it's the Apple Store, it doesn't even crack when you run into it.  You try using various tools and objects you find around the store and in the back and manage to shatter the glass, but as soon as it shatters, the security alarm goes off and a security gate drops down in front of the entrance, trapping you inside and soon found by the security guard.");
         progressStage(5);
     }
     
-    function dj() {
-        logMessage("You decide to kill some time and manage to connect the store speakers over the intercom you find behind the counter of the Genius Bar and start rapping one of your favorite songs. Soon, a security guard appears and after explaining your situation, he lets you out!");
+    function backDoor() {
+        logMessage("You try unlocking the back door, but the key doesn't turn.");
+        progressStage(4);
+    }
+    
+    function frontDoor() {
+        logMessage("You try unlocking the front entrance door and this time, the key turns and unlocks! You're free! After returning the key to where you found it, you leave happily.");
         progressStage(6);
     }
     
@@ -101,6 +108,11 @@ document.addEventListener("DOMContentLoaded", () => {
     function ceilingVent() {
         logMessage("You manage to climb up onto a store table and manage to reach the ceiling where you push up a tile and pull yourself into the vents, but because the tiles are thin and you are too heavy for them to hold, you break through the ceiling and fall back into the store where a security guard soon appears and finds you.");
         progressStage(5);
+    }
+    
+    function dj() {
+        logMessage("You decide to kill some time and manage to connect the store speakers over the intercom you find behind the counter of the Genius Bar and start rapping one of your favorite songs. Soon, a security guard appears and after explaining your situation, he lets you out!");
+        progressStage(6);
     }
     
     function callCook() {
@@ -115,8 +127,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("useMac").addEventListener("click", useMac);
     document.getElementById("storePhone").addEventListener("click", storePhone);
     document.getElementById("breakGlass").addEventListener("click", breakGlass);
-    document.getElementById("dj").addEventListener("click", dj);
+    document.getElementById("backDoor").addEventListener("click", backDoor);
+    document.getElementById("frontDoor").addEventListener("click", frontDoor);
     document.getElementById("ceilingVent").addEventListener("click", ceilingVent);
+    document.getElementById("dj").addEventListener("click", dj);
     document.getElementById("callCook").addEventListener("click", callCook);
     document.getElementById("resetGame").addEventListener("click", restartGame);
     document.getElementById("backButton").addEventListener("click", () => {
