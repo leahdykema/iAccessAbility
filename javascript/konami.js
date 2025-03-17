87066f033083f60c9ea6bd808a37a8d08119c171
Leah Dykema <103963317+leahdykema@users.noreply.github.com>
2025-03-16 21:15:01 -0500

    Update konami.js
    Signed-off-by: Leah Dykema <103963317+leahdykema@users.noreply.github.com>

--- javascript/konami.js
+++ javascript/konami.js
@@ -172,7 +172,7 @@ popupStyles.textContent = `
         }
     }
     
-    @media (max-width: 1100px) {
+    @media (max-width: 1500px) {
         #konami-icon {
             display: none !important;
         }
